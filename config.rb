4c7fe83310e517deeef53e0b1d19370694f38dcb
Alexey Porotnikov <user@deuter.homenet>
2013-03-19 17:26:38 +0600

    Dumb post

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/alporeadings/"
+http_images_path = "/alporeadings/images"
+http_fonts_path = "/alporeadings/fonts"
+css_dir = "public/alporeadings/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
